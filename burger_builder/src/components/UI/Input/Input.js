5d0c6ee5e38d6a662f77a2c26798e58113930f7a
VictorSRY <61047218+VictorSRY@users.noreply.github.com>
2020-12-11 08:35:30 +0530

    ch14

--- burger_builder/src/components/UI/Input/Input.js
+++ burger_builder/src/components/UI/Input/Input.js
@@ -2,15 +2,15 @@ import css from "./Input.module.css"
 const Input =(props)=>{
     let inputElement=null
     switch(props.elementType){
-        case ('input') : inputElement=<input className={css.InputElement} {...props.elementConfig}/>;
+        case ('input') : inputElement=<input className={css.InputElement} onChange={props.change} {...props.elementConfig}/>;
         break; 
-        case ('textArea') : inputElement=<input className={css.InputElement} {...props.elementConfig}/>;
+        case ('textArea') : inputElement=<input className={css.InputElement} onChange={props.change} {...props.elementConfig}/>;
         break; 
-        case ('select') : inputElement=<select name={props.elementConfig.name}>
-            {props.elementConfig.options.map(data=>(<option value={props.value}>{props.diplay}</option>))}
+        case ('select') : inputElement=<select name={props.elementConfig.name} onChange={props.change} >
+            {props.elementConfig.options.map(data=>(<option value={data.value} key={data.value}>{data.display}</option>))}
         </select>;
         break; 
-        default : inputElement=<input className={css.InputElement} {...props.elementConfig}/>; 
+        default : inputElement=<input className={css.InputElement} onChange={props.change} {...props.elementConfig}/>; 
     }
     return(
         <div className={css.Input}>
--- burger_builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger_builder/src/containers/Checkout/ContactData/ContactData.js
@@ -38,19 +38,30 @@ export default class ContactData extends Component{
                     options:[{value:'fastest',display:'fastest'},{value:'cheapest',display:'cheapest'}],
                 }
             },
-        },
-        eamil:'',
-        address:'',
+        }, 
+        formData:{
+            address: "",
+            deliveryType: "fastest",
+            email: "",
+            name: "",
+        }
     }
     CDSubmitHandler=(event)=>{
         event.preventDefault();
-        oHttp.post('/orders.json', {...this.state,ingredients:this.props.order}).then(response => { console.log(response); this.props.history.replace('/') }).catch(error => { console.log(error); })
+        oHttp.post('/orders.json', {...this.state.formData,ingredients:this.props.order}).then(response => { console.log(response); this.props.history.replace('/') }).catch(error => { console.log(error); })
+    }
+    inputValueHandler=(event)=>{
+        console.log(event.target.name,event.target.value)
+        const formData =this.state.formData
+        console.log(formData)
+        formData[event.target.name]=event.target.value
+        this.setState({formData:formData})
     }
     render(){
         const elementArray=[]
         for(let key in this.state.form){
             const temp =this.state.form[key]
-            elementArray.push(<Input elementType={temp.elementType} elementConfig={temp.elementConfig} key={key}/>)
+            elementArray.push(<Input elementType={temp.elementType} elementConfig={temp.elementConfig} change={this.inputValueHandler} key={key}/>)
         }
         return(
             <div className={css.ContactData}>

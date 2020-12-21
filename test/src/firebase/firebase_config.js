79f8b37c03b34090d266fba917a86948513ea657
VictorSRY <61047218+VictorSRY@users.noreply.github.com>
2020-12-21 11:26:42 +0530

    Update firebase_config.js

---
+++ test/src/firebase/firebase_config.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAsWDzOoqudlohP2Ld-stE8_ZfJJ5LLzGo",
+    authDomain: "react-blogger-app.firebaseapp.com",
+    databaseURL: "https://react-blogger-app-default-rtdb.firebaseio.com",
+    projectId: "react-blogger-app",
+    storageBucket: "react-blogger-app.appspot.com",
+    messagingSenderId: "449820954296",
+    appId: "1:449820954296:web:feec99c871da1b74a7ee4d",
+    measurementId: "G-9SS9JYLR87"
+  };
